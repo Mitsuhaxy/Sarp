8d325402eee11fa6552239097d43ff1938788909
悠 <i@mengxy.me>
2021-07-19 11:09:48 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@
 set -e 
 ifaddress=$(ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:")
 ifgateway=$(route -n | grep eth0 | grep UG | awk '{print $2}')
+ip link add wgcf type wireguard
+echo "0" >> /proc/sys/net/ipv6/conf/wgcf/disable_ipv6
 wg-quick up /etc/wireguard/wgcf.conf
 echo "252     eth0" >> /etc/iproute2/rt_tables
 ip rule add from $ifaddress lookup eth0
