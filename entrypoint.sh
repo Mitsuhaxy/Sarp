192e2cf14f0454c3f32dbea5c59a793e0bd98322
悠 <i@mengxy.me>
2021-07-22 19:44:37 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,17 @@
 #!/bin/sh 
 
-set -e 
+set -e
 ifaddress=$(ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:")
-ifgateway=$(route -n | grep eth0 | grep UG | awk '{print $2}')
-wg-quick up /etc/wireguard/wgcf.conf
+if6address=$(ifconfig -a|grep inet6|grep -v ::1|awk '{print $2}')
+ifgateway=$(ip route | grep 'default' | awk '{print $3}')
+cp /etc/wireguard/wgcf.conf /wgcf.conf
+sed -i '/MTU = 1280/a\Table = off'  /wgcf.conf
+ip route add 162.159.192.1 via $ifgateway
+wg-quick up /wgcf.conf
 echo "252     eth0" >> /etc/iproute2/rt_tables
 ip rule add from $ifaddress lookup eth0
-ip route add table eth0 default via $ifgateway
+ip route del default
+ip route add default dev wgcf
+ip route add table eth0 default via $ifgateway dev eth0
+ip -6 route add default dev wgcf 
 ss-server -s $ifaddress -p 8388 -k $key -m $method -u -d $dns
