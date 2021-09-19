2fa9b0e3b681268c37d36c30cf02c3afd89fb5b1
悠 <i@mengxy.me>
2021-09-20 04:01:43 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 set -e
 ifaddress=$(ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:")
 ifgateway=$(ip route | grep 'default' | awk '{print $3}')
-cp /etc/wireguard/wgcf.conf /etc/wireguard/sarp.conf
-sed -i '/MTU = 1280/a\Table = off'  /etc/wireguard/sarp.conf
+mkdir /var/wireguard && cp /etc/wireguard/wgcf.conf /var/etc/wireguard/wgcf.conf
+sed -i '/MTU = 1280/a\Table = off'  /var/etc/wireguard/wgcf.conf
 ip route add 162.159.192.1 via $ifgateway
 wg-quick up /etc/wireguard/sarp.conf
 echo "252     eth0" >> /etc/iproute2/rt_tables
